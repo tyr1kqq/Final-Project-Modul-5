8e53abc790dfc06f8c3fbe8e23c0c70b9a4e9b4a
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-29 10:13:49 +0200

    Добавьте файлы проекта.

---
+++ Final Project Modul 5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_Project_Modul_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
